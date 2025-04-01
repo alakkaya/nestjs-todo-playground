0c3a93977b268b9315fa3ab52e66da5b041de8af
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-04-01 21:51:42 +0300

    feat(user): implement user model and repository for MongoDB integration

--- src/modules/user/model/user.model.ts
+++ src/modules/user/model/user.model.ts
@@ -1,8 +1,32 @@
-export class User {
-  id: string;
-  fullname: string;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema, Types } from 'mongoose';
+import { User } from 'src/core/interface/mongo-model';
+
+export type UserDocument = UserModel & Document;
+
+@Schema({ timestamps: true })
+export class UserModel implements User {
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    required: false,
+    default: Types.ObjectId,
+  })
+  public _id: string;
+
+  @Prop({ type: String, required: true })
+  fullName: string;
+
+  @Prop({ type: String, unique: true, required: true })
   nickname: string;
+
+  @Prop({
+    type: String,
+    select: false, // Do not include password in the response
+    minlength: 6,
+    maxlength: 50,
+    required: true,
+  })
   password: string;
-  createdAt: Date;
-  updatedAt: Date;
 }
+
+export const UserSchema = SchemaFactory.createForClass(UserModel);
--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,1 +1,19 @@
-export * from './user.repository';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserDocument, UserModel } from '../model/user.model';
+import { Model } from 'mongoose';
+import { CreateUserDto } from '../dto';
+import { User } from 'src/core/interface/mongo-model';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectModel(UserModel.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDTO: CreateUserDto): Promise<User> {
+    const newUser = new this.userModel(createUserDTO);
+    return newUser.save();
+  }
+}
