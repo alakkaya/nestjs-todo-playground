d3a8386134be8cca4c6a15aa9ac7d0cad1044140
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-04-01 18:58:29 +0300

    refactor user module: restructure files, move controller and service to dedicated directories, and add initial tests

---
+++ src/modules/user/controller/user.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { UserService } from '../service';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
 
 @Module({
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class UserModule {}
