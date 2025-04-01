bab0a6a23353e1c518eb7a66e70f00efea722597
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-04-01 22:45:41 +0300

    feat(user): implement user creation endpoint and service logic

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../service';
+import { User } from 'src/core/interface/mongo-model';
+import { CreateUserDto } from '../dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto);
+  }
 }
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository';
+import { CreateUserDto } from '../dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async create(user: CreateUserDto) {
+    const existingUser = await this.userRepository.findByNickname(
+      user.nickname,
+    );
+    if (existingUser) {
+      throw new Error('Bu kullanıcı adı zaten kullanılıyor');
+    }
+    return this.userRepository.create(user);
+  }
+}
