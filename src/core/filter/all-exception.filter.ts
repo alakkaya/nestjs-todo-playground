25a2acfc72ae152297aae37eaf8be7ba7ae3c2b2
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-05-12 19:57:35 +0300

    fix(errors): reorder error codes and update default error code for BadInputException

--- src/core/filter/all-exception.filter.ts
+++ src/core/filter/all-exception.filter.ts
@@ -1,4 +1,9 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
 import { GeneralServerException } from '../error';
 
 @Catch()
@@ -8,11 +13,32 @@ export class AllExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse();
     const request = ctx.getRequest();
 
+    // Eğer custom error değilse ama NestJS HttpException ise
+    if (!exception.isCustomError && exception instanceof HttpException) {
+      const status = exception.getStatus();
+      const responseBody = exception.getResponse();
+      response.status(status).json({
+        meta: {
+          headers: request.headers,
+          params: request.params,
+          status,
+          errorCode: null,
+          errorMessage:
+            typeof responseBody === 'string'
+              ? responseBody
+              : responseBody['message'],
+          timestamp: new Date().toISOString(),
+        },
+        result: responseBody,
+      });
+      return;
+    }
+
     if (!exception.isCustomError) {
       exception = new GeneralServerException();
     }
 
-    response.status(500).json({
+    response.status(exception.httpStatusCode ?? 500).json({
       meta: {
         headers: request.headers,
         params: request.params,
