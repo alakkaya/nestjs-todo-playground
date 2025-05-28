253a56dde71b9f0a8e25d66b6ba6ee1b72df40e2
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-05-28 19:06:11 +0300

    feat(auth, user): integrate Swagger for API documentation and enhance DTOs with ApiProperty decorators

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -17,23 +17,48 @@ import {
 import { AuthGuard } from 'src/core/guard/auth.guard';
 import { ReqUser } from 'src/core/decorator';
 import { User } from 'src/core/interface/mongo-model';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @ApiOperation({ summary: 'Sign in a user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User signed in successfully.',
+    type: SignInAck,
+  })
   async signIn(@Body() signInDto: SignInDto): Promise<SignInAck> {
     return this.authService.signIn(signInDto);
   }
 
   @Post('sign-out')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Sign out a user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User signed out successfully.',
+  })
   async signOut(@ReqUser() user: User): Promise<void> {
     return this.authService.signOut(user._id);
   }
 
   @Post('refresh-token')
+  @ApiOperation({ summary: 'Refresh access token using refresh token' })
+  @ApiResponse({
+    status: 201,
+    description: 'Access token refreshed successfully.',
+    type: RefreshTokenAck,
+  })
   async refreshToken(
     @Body() refreshTokenDto: RefreshTokenDto,
   ): Promise<RefreshTokenAck> {
@@ -42,6 +67,13 @@ export class AuthController {
 
   @Get('me')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get current user information' })
+  @ApiResponse({
+    status: 200,
+    description: 'Current user information retrieved successfully.',
+    type: MeAck,
+  })
   async me(@ReqUser() user: User): Promise<MeAck> {
     return {
       _id: user._id,
